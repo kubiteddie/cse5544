3e4ca203e9c80402d6ac9f84709ff507af5b0fb1
kubiteddie <eddiekubit0@gmail.com>
2024-02-07 15:00:39 -0500

    lab 4 parts a, b

---
+++ lab4/README.md
@@ -0,0 +1,17 @@
+# Eddie Kubit
+### CSE 5544 Lab 4
+### Jian Chen, Tu-Th 11:10
+
+<hr/>
+
+### Installation
+
+#### - VSCode
+#### - LiveServer Extension on VSCode
+#### - Install node.js
+#### - Run `npm install d3`
+
+Once unzipped, navigate to the base directory unzipped in VSCode. Open `lab2.kubit.7.html` in the editor, and click the "Go Live" button in the bottom left corner of the VSCode UI. This button should appear once you install and enable the LiveServer extension. This should open the html file in a browser window. If a directory is opened in the browser window, navigate to the file or the directory where the file is stored. This will show the html file with all of the required scatter plots.
+
+*Note - radii of points are scaled up by a factor of 2 for visibility. The uncertainty is realtive, so this is not a problem.*
+
---
+++ lab4/script1.js
@@ -0,0 +1,67 @@
+const datalocation = 'GlobalTemperatures.json'
+
+function numToDate(num){
+        let year = Math.floor(num/10000)
+        let day = num % 100
+        let month = Math.floor(num/100) % 100
+        return new Date(year, month-1, day)
+}
+
+
+let data = d3.json(datalocation)
+    .then(function(data) {
+        const svgScatter = d3.select('#scatterplot_1');
+        const marginScatter = { top: 20, right: 20, bottom: 45, left: 60 };
+        const widthScatter = +svgScatter.attr("width") - marginScatter.left - marginScatter.right;
+        const heightScatter = +svgScatter.attr("height") - marginScatter.top - marginScatter.bottom;
+
+        const cxScale = d3.scaleTime()
+                .domain([numToDate(d3.min(data, d => d.time)), numToDate(d3.max(data, d => d.time))])
+                .range([0, widthScatter]);
+
+        const cyScale = d3.scaleLinear()
+                .domain([d3.min(data, d => d.LAT), d3.max(data, d => d.LAT)])
+                .range([heightScatter, 0]);
+
+        const rScale = d3.scaleSqrt()
+                .domain([d3.min(data, d => d.LATU), d3.max(data, d => d.LATU)])
+                .range([4,8])
+
+        const gScatter = svgScatter.append("g")
+                .attr("transform", "translate(" + marginScatter.left + "," + marginScatter.top + ")");
+
+        gScatter.selectAll("circle")
+                .data(data)
+                .enter().append("circle")
+                .attr("cx", d => cxScale(numToDate(d.time)))
+                .attr("cy", d => cyScale(d.LAT))
+                .attr("r", d => rScale(d.LATU))
+                .attr("fill", "black");
+
+        gScatter.append("g")
+                .attr("class", "x-axis")
+                .attr("transform", "translate(0," + heightScatter + ")")
+                .call(d3.axisBottom(cxScale));
+
+        gScatter.append("g")
+                .attr("class", "y-axis")
+                .call(d3.axisLeft(cyScale));
+
+        gScatter.append("text")
+                .attr("class", "x label")
+                .attr("text-anchor", "end")
+                .attr("x", widthScatter / 2 + 20)
+                .attr("y", heightScatter + 33)
+                .text("Time (s)");
+
+        gScatter.append("text")
+                .attr("class", "y label")
+                .attr("text-anchor", "end")
+                .attr("transform", "rotate(-90)")
+                .attr("y", -marginScatter.left + 20)
+                .attr("x", -marginScatter.top - 90)
+                .text("Land Average Temperature (Degrees C)")
+    })
+    .catch(function(error) {
+        console.error("Error loading JSON file:", error);
+    });
